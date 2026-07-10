2c15fca52b84385733133db387c9a10b6e4f264e
温州程序员劝退师 <adaihahaha@hotmail.com>
2026-07-11 00:32:48 +0800

    feat: enable Vercel Web Analytics

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { inject } from "@vercel/analytics";
 import Phaser from "phaser";
 import "./style.css";
 import { GameSimulation } from "./game/simulation/GameSimulation";
@@ -5,6 +6,8 @@ import { AudioController } from "./game/audio/AudioController";
 import { BootScene } from "./phaser/scenes/BootScene";
 import { GameScene } from "./phaser/scenes/GameScene";
 import { HudController } from "./ui/HudController";
+
+inject();
 
 const simulation = new GameSimulation();
 const audio = new AudioController();
